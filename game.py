937bace8e55ea15abf01a8620cf7681354cbd77c
BuildTools <unconfigured@null.spigotmc.org>
2022-02-27 18:18:38 -0300

    Added comments and removed uneeded lines

--- game.py
+++ game.py
@@ -1,44 +1,76 @@
 import random
 
+#This module contains the actual "game" and its functions
 
-def make_guess(password:str, guess:str):
+#Checks a guess with a password and returns the result. Ex.: [0,1,0,2,1]
+# 0 is gray (wrong letter)
+# 1 is yellow (wrong position)
+# 2 is green (correct)
+def make_guess(password:str, guess:str) -> list:
+    #Changes the strings into lists of strings
     guess = list(guess)
     password = list(password)
     letters = list(password)
+    
     result = [0,0,0,0,0]
 
+    #Checks all the green letters
     for idx,val in enumerate(guess):
         if val == password[idx]:
             result[idx] = 2
+            #Removes the green letters from the possible pool, so it doesn't count twice when yellow
             if val in letters: letters.remove(val)
 
+    #Chacks all the yellow letters
     for idx,val in enumerate(guess):
         if result[idx] == 0 and val in letters:
             result[idx] = 1
+            #Also removes from the pool so it's not counted twice
             letters.remove(val)
 
     return result
 
-def play(password:str, game:list, words:list[str]):
+#Tries to match a pattern to a given password
+def play(password:str, game:list, words:list[str]) -> list:
+    
+    #Sorts the lines from most to least points (sum of the letters). It is easier to try more restrictive patterns first (the ones that are more correct).
     sorted_game = sorted(game, key=lambda li: sum(li[1]), reverse=True)
+
+    #Shuffles the possible words and copies it. (One of them will be altered, but we still need the original)
     random.shuffle(words)
     filtered_words = words.copy()
-    all_banned = []
+
+    all_banned = [] #All banned letters (These are letters that have been gray already)
     results = []
+
     for li in sorted_game:
+        #Tries to find a word that gives the desired pattern without using banned letters
         guess = next((x for x in filtered_words if make_guess(password, x) == li[1]), None)
+
+        #If it fails, tries the original pool even if it uses invalid letters
         if guess == None:
             guess = next((x for x in words if make_guess(password, x) == li[1]), None)
-            if guess == None: return None
+            if guess == None: return None #If it still fails, we give up on this password
+        else:
+            filtered_words.remove(guess)
+
+        #Removes the guess so it doesn't try it again on this game
+        words.remove(guess)
+
+        #Ban gray letters in guess (letters that are not in the password)
         banned = [x for x in list(guess) if x not in list(password)]
+
         results.append([li[0],guess])
         all_banned.extend(banned)
-        random.shuffle(words)
+
+        #Filter words that have banned letters
         filtered_words = list(filter(lambda w: not [l for l in list(w) if l in all_banned], words))
+    
+    #Sort the result to go back to its original order
     return sorted(results, key=lambda li: li[0])
 
+#Plays a game for each possible password
 def playMultiple(passwords:list[str], game:list, words:list[str]):
-    random.shuffle(passwords)
     results = []
     for pswd in passwords:
         result = play(pswd, game, words)
--- wordle.py
+++ wordle.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import game as gm
 
-
+#This class manages the options and file input and output
 class Wordle:
 
     def __init__(self, pool:str, pool_dir:str, pattern_dir:str) -> None:
@@ -13,28 +13,41 @@ class Wordle:
         self.__set_patterns()
         self.__set_words()
 
+    #Reads the patterns from the files
     def __set_patterns(self) -> pd.DataFrame:
+        #Gets every child directory from the patterns dir
         pattern_dirs = [self.patterns_pth + '/' + x for x in next(os.walk(self.patterns_pth))[1]]
+        
         patterns = []
         for p_dir in pattern_dirs:
+            #Gets only the dir name
             p_name = os.path.basename(p_dir)
             p_file = p_dir + '/pattern.txt'
+            
             lines = []
             with open(p_file) as file:
                 for idx,line in enumerate(file):
+                    #Creates an array for each line in the file with its index. Ex.: [0,[0,1,1,0,2]]
                     lines.append([idx, [int(l) for l in list(line.rstrip())]])
+            
+            #Array with the name of the pattern and the pattern itself
             patterns.append([p_name, lines])
+        
+        #Makes a DataFrame with named columns just to make things easier
         self.patterns = pd.DataFrame(patterns, columns=['name','pattern'])
     
+    #Reads the words from the pools
     def __set_words(self) -> None:
         answers = []
         words = []
 
+        #Possible answers are used in both answers(passwords) and words(guesses)
         with open(self.answers_pth) as file:
             for line in file:
                 answers.append(line.rstrip())
                 words.append(line.rstrip())
 
+        #Allowed guesses populate only the words
         with open(self.allowed_pth) as file:
             for line in file:
                 words.append(line.rstrip())
@@ -42,45 +55,51 @@ class Wordle:
         self.answers = answers
         self.words = words
     
+    #Generates solutions and writes them to a file
     def play(self, pattern_name:str, attempts:int=1, repeat_pwd:bool=True, trace:bool=True) -> None:
 
+        #Finds the chosen pattern
         pattern_rows = self.patterns.loc[self.patterns['name'] == pattern_name]['pattern']
         if pattern_rows.empty:
             if trace: print('Pattern named ' + pattern_name + ' not found.')
             return
-        
         pattern = pattern_rows.iloc[0]
 
+        #Sets the solutions file path
         patternf_path = self.patterns_pth + '/' + pattern_name + '/' + self.pool + '_' + ('repeat' if repeat_pwd else 'distinct') + '.csv'
 
         if trace: print('Generating solutions...')
         for i in range(0, attempts):
             if trace: print('Attempt ' + str(i+1))
 
+            #Generates the possible solutions
             possible_games = gm.playMultiple(self.answers, pattern, self.words)
 
+            #Separates the pattern from the guesses and changes them into tuples for later
             pgames_array = []
-
             for pg in possible_games:
-                pgames_array.append([pg[0],tuple([x[1] for x in pg[1]])])
-
+                pgames_array.append([pg[0], tuple([x[1] for x in pg[1]])])
             pgames_df = pd.DataFrame(pgames_array, columns=['password','game'])
 
+            #Creates directories if something doesn't exist
             os.makedirs(os.path.dirname(patternf_path), exist_ok=True)
 
-            
+            #Creates the solutions file if it doesn't exist
             f = open(patternf_path, 'a')
             if os.stat(patternf_path).st_size <= 0:
                 f.write('password,game')
             f.close()
                 
-
+            #Reads file to join the results and remove duplicates.
             pgames_file = pd.read_csv(patternf_path)
-
             pgames_df = pd.concat([pgames_df,pgames_file])
+            #It normally checks both the password and the game to define duplicates, but can check the password alone if desired.
             pgames_df = pgames_df[~pgames_df.duplicated(subset=['password','game'] if repeat_pwd else ['password']).values]
+
+            #Sorts alfabetically
             pgames_df = pgames_df.sort_values(by='password')
 
+            #Writes to file
             pgames_df.to_csv(patternf_path, index=False)
 
             if trace: print('File updated')
